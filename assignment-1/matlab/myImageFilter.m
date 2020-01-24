6f6c23ba9e5b54b6c6e69c6eeef90139219cb75f
Ali Gholami <gholami@sfu.ca>
2020-01-24 12:47:12 -0800

    Update: Working convolution (without kernel flip)

---
+++ assignment-1/matlab/myImageFilter.m
@@ -0,0 +1,31 @@
+function [img1] = myImageFilter(img0, h)
+
+    [num_img_rows, num_img_cols] = size(img0);
+    [num_filter_rows, num_filter_cols] = size(h);
+    pad_value = floor(num_filter_rows / 2);
+
+    % Pad the image (post and pre)
+    img0_padded = padarray(img0, [pad_value, pad_value], 'replicate', 'pre');
+    img0_padded = padarray(img0_padded, [pad_value, pad_value], 'replicate', 'post');
+    returning_img = img0_padded;
+    
+    % Filter amplitude
+    amp = 169
+
+    [num_padded_img_rows, num_padded_img_cols] = size(img0_padded);
+    for i=1:num_padded_img_rows
+        for j=1:num_padded_img_cols
+            acc = 0;
+            for m=1:num_filter_rows
+                for n=1:num_filter_cols
+                    acc = acc + h(m, n) * img0_padded(i, j);
+                end
+            end
+            returning_img(i + pad_value, j + pad_value) = acc / amp;
+        end
+    end
+
+    % Unpad the image
+    [ret_img_num_rows, ret_img_num_cols] = size(returning_img);
+    img1 = returning_img(pad_value:ret_img_num_rows - 1, pad_value:ret_img_num_cols - 1);
+end
