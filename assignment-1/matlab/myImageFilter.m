d41e31a26cb09591209dfaefbaad680fa79afab5
Ali Gholami <gholami@sfu.ca>
2020-01-24 20:15:15 -0800

    Update: Flip Kernel Before Conv

--- assignment-1/matlab/myImageFilter.m
+++ assignment-1/matlab/myImageFilter.m
@@ -1,5 +1,6 @@
 function [img1] = myImageFilter(img0, h)
 
+    h = fliplr(h)
     [num_img_rows, num_img_cols] = size(img0);
     [num_filter_rows, num_filter_cols] = size(h);
     pad_value = floor(num_filter_rows / 2);
@@ -10,18 +11,19 @@ function [img1] = myImageFilter(img0, h)
     returning_img = img0_padded;
     
     % Filter amplitude
-    amp = 169
+    amp = sum(h, 'all');
 
     [num_padded_img_rows, num_padded_img_cols] = size(img0_padded);
-    for i=1:num_padded_img_rows
-        for j=1:num_padded_img_cols
+
+    for i=1:num_padded_img_rows - num_filter_rows
+        for j=1:num_padded_img_cols - num_filter_cols
             acc = 0;
             for m=1:num_filter_rows
                 for n=1:num_filter_cols
-                    acc = acc + h(m, n) * img0_padded(i, j);
+                    acc = acc + h(m, n) * img0_padded(i + m, j + n) / amp;
                 end
             end
-            returning_img(i + pad_value, j + pad_value) = acc / amp;
+            returning_img(i + pad_value, j + pad_value) = acc;
         end
     end
 
