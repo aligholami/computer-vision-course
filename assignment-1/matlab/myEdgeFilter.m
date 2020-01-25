de5c87f4283cf0db5f80e2405af0bab1a9bbace5
Ali Gholami <gholami@sfu.ca>
2020-01-24 22:42:21 -0800

    Update: Adaptive kernel size for Gaussian kernel

--- assignment-1/matlab/myEdgeFilter.m
+++ assignment-1/matlab/myEdgeFilter.m
@@ -1,6 +1,7 @@
 function [img1] = myEdgeFilter(img0, sigma)
     % Get the Gaussian filter
-    kernel = fspecial('gaussian', [5 5], sigma);
+    kernel_size = 2 * ceil(3 * sigma) + 1
+    kernel = fspecial('gaussian', [kernel_size kernel_size], sigma);
 
     img_smoothed = myImageFilter(img0, kernel);
     img1 = img_smoothed
