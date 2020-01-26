d7f7bf6ab1b29bbf3706946f2c29b2d9c99f70e9
Ali Gholami <gholami@sfu.ca>
2020-01-26 15:06:39 -0800

    Update: Added Line Detector

--- assignment-1/matlab/myHoughLines.m
+++ assignment-1/matlab/myHoughLines.m
@@ -1,5 +1,31 @@
 function [rhos, thetas] = myHoughLines(H, nLines)
-%Your implemention here
 
+    [num_rows, num_cols] = size(H);
+    threshold = 0.4 * max(H(:))
+    receptive_field_x_axis = floor(size(H, 1) / 100.0) * 2 + 1
+    receptive_field_y_axis = floor(size(H, 2) / 100.0) * 2 + 1
+    
+    p_rhos = []
+    p_thetas = []
+    for i=1:nLines
+        H_sorted = sort(H(:), 'descend');
+        peak_value = H_sorted(1);
+        if peak_value >= threshold
+            [row, col] = find(H == peak_value, 1)
+
+            % Non Maximum Suppression along x and y
+            x_low = max([floor(row-receptive_field_y_axis) 1]);
+            x_high = min([ceil(row+receptive_field_y_axis) num_rows]);
+            y_low = max([floor(col-receptive_field_x_axis) 1]);
+            y_high = min([ceil(col+receptive_field_x_axis) num_cols]);
+
+            H(x_low:x_high,y_low:y_high) = 0;
+
+            p_rhos = [p_rhos; row]
+            p_thetas = [p_thetas; col]
+        end
+    end
+    rhos = p_rhos
+    thetas = p_thetas
 end
         
--- assignment-1/matlab/myHoughTransform.m
+++ assignment-1/matlab/myHoughTransform.m
@@ -1,19 +1,19 @@
 function [H, rhoScale, thetaScale] = myHoughTransform(Im, threshold, rhoRes, thetaRes)
-    Im(Im < threshold) = 0
+    Im(Im < threshold) = 0;
     [num_rows, num_cols] = size(Im);
     theta_max = pi;
     rho_max = ceil(sqrt(num_cols^2 + num_rows^2));
     thetaScale = 0:thetaRes:theta_max;
     rhoScale = -rho_max:rhoRes:rho_max;
     H = zeros(2*rho_max/rhoRes+1, pi/thetaRes+1);
-    cos_theta = cos(thetaScale)
-    sin_theta = sin(thetaScale)
+    cos_theta = cos(thetaScale);
+    sin_theta = sin(thetaScale);
            
     for i=1:num_rows
         for j=1:num_cols
             if Im(i, j) > 0
-                y = i
-                x = j
+                y = i;
+                x = j;
                 for theta_idx=1:size(thetaScale, 2)
                     rho = x * cos_theta(theta_idx) + y * sin_theta(theta_idx);
                     rho_idx = floor((rho + rho_max) / rhoRes) + 1;
@@ -22,5 +22,5 @@ function [H, rhoScale, thetaScale] = myHoughTransform(Im, threshold, rhoRes, the
             end
         end
     end
-    H = H / 255
+    H = H / 255;
 end
