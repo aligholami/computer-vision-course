5163181a2a5ce387839dc998b2c50ed27e79a248
Ali Gholami <gholami@sfu.ca>
2020-02-08 14:32:34 -0800

    Update: Computes Homography between point pairs

--- assignment-2/matlab/computeH.m
+++ assignment-2/matlab/computeH.m
@@ -1,3 +1,20 @@
 function [ H2to1 ] = computeH( x1, x2 )
 %COMPUTEH Computes the homography between two sets of points
+
+num_crspds = size(x1, 1);
+A = [];
+if num_crspds >= 4
+    for i=1:4
+        A_i = [-x1(i, 1), -x1(i, 2), -1, 0, 0, 0, x1(i, 1)*x2(i, 1), x1(i, 2)*x2(i, 1), x2(i, 1); 0, 0, 0, -x1(i, 1), -x1(i, 2), -1, x1(i, 1)*x2(i, 2), x1(i, 2)*x2(i, 2), x2(i, 2)];
+        A = [A; A_i];
+    end
+end
+
+[U, S, V] = svd(A);
+s = svds(A);
+[val, idx] = min(s);
+h = V(:, idx);
+
+H2to1 = reshape(h, [3, 3])';
+
 end
--- assignment-2/matlab/test43.m
+++ assignment-2/matlab/test43.m
@@ -13,14 +13,19 @@ im2_corners = detectFASTFeatures(im2_gray);
 [im1_desc, im1_locs] = computeBrief(im1_gray, im1_corners.selectStrongest(100).Location);
 [im2_desc, im2_locs] = computeBrief(im2_gray, im2_corners.selectStrongest(100).Location);
 
-%% Match features using the descriptors
 index_pairs = matchFeatures(im1_desc, im2_desc, 'MatchThreshold', 10, 'MaxRatio', 0.9);
+H = computeH(im1_locs, im2_locs);
 
-locs1 = im1_locs(index_pairs(:, 1), :);
-locs2 = im2_locs(index_pairs(:, 2), :);
+locs1 = im1_locs(index_pairs(:, 1), :); % [N, 3]
+locs1_homo = [locs1, ones(size(locs1, 1), 1)]';   % [3, N]
+locs1_Hed = round(H * locs1_homo)';
 
-figure;
-showMatchedFeatures(I1, I2, locs1, locs2, 'montage');
+locs1
+locs1_Hed = locs1_Hed(:, 1:2)
+
+% figure;
+% showMatchedFeatures(I1, I2, locs1, locs2, 'montage');
+
 
 end
 
