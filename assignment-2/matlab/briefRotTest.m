75992d7ac92d00f5718b3ab940e73e75827748e0
Ali Gholami <gholami@sfu.ca>
2020-02-08 13:10:40 -0800

    Update: Added SURF feature extraction bar plot

--- assignment-2/matlab/briefRotTest.m
+++ assignment-2/matlab/briefRotTest.m
@@ -23,11 +23,36 @@ for i = 0:36
     
     num_matches = size(index_pairs, 1); 
     
-    num_matches
     %% Update histogram
     histo_y = [histo_y num_matches];
 end
 
 bar(histo_x, histo_y)
+
+
+%% Compute the features and descriptors
+cv_cover_corners = detectSURFFeatures(cv_cover_gray);
+[cv_cover_desc, cv_cover_locs] = extractFeatures(cv_cover_gray, cv_cover_corners.selectStrongest(100).Location, 'Method', 'SURF');
+
+histo_x = 0:10:360;
+histo_y = [];
+for i = 0:36
+    %% Rotate image
+    rot = imrotate(cv_cover_gray, i * 10);
+
+    %% Compute features and descriptors
+    rot_corners = detectSURFFeatures(rot);
+    [rot_desc, rot_locs] = extractFeatures(rot, rot_corners.selectStrongest(100).Location, 'Method', 'SURF');
+
+    %% Match features
+    index_pairs = matchFeatures(rot_desc, cv_cover_desc, 'MatchThreshold', 10, 'MaxRatio', 0.9);
+    
+    num_matches = size(index_pairs, 1); 
+    
+    %% Update histogram
+    histo_y = [histo_y num_matches];
+end
+
+bar(histo_x, histo_y);
 
 %% Display histogram
