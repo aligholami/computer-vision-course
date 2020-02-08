fe24f94aace8b571f7fa2fed75ecad2f8f96eb64
Ali Gholami <gholami@sfu.ca>
2020-02-08 13:02:23 -0800

    Update: Added BRIEF rot test

--- assignment-2/matlab/briefRotTest.m
+++ assignment-2/matlab/briefRotTest.m
@@ -1,17 +1,33 @@
 % Your solution to Q2.1.5 goes here!
 
 %% Read the image and convert to grayscale, if necessary
+cv_cover_gray = imread('./data/cv_cover.jpg');
+% cv_cover_gray = rgb2gray(cv_cover);
 
 %% Compute the features and descriptors
+cv_cover_corners = detectFASTFeatures(cv_cover_gray);
+[cv_cover_desc, cv_cover_locs] = computeBrief(cv_cover_gray, cv_cover_corners.selectStrongest(100).Location);
 
+histo_x = 0:10:360;
+histo_y = [];
 for i = 0:36
     %% Rotate image
-    
+    rot = imrotate(cv_cover_gray, i * 10);
+
     %% Compute features and descriptors
-    
+    rot_corners = detectFASTFeatures(rot);
+    [rot_desc, rot_locs] = computeBrief(rot, rot_corners.selectStrongest(100).Location);
+
     %% Match features
+    index_pairs = matchFeatures(rot_desc, cv_cover_desc, 'MatchThreshold', 10, 'MaxRatio', 0.9);
     
+    num_matches = size(index_pairs, 1); 
+    
+    num_matches
     %% Update histogram
+    histo_y = [histo_y num_matches];
 end
+
+bar(histo_x, histo_y)
 
 %% Display histogram
