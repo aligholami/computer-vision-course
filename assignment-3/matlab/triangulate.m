e22fa979135a8b89f8097fa6873eb7f815920a6d
Ali Gholami <gholami@sfu.ca>
2020-03-03 12:50:23 -0800

    Update: Latest triangulate

--- assignment-3/matlab/triangulate.m
+++ assignment-3/matlab/triangulate.m
@@ -1,45 +1,31 @@
-function pts3d = triangulate(P1, pts1, P2, pts2)
-    % triangulate estimate the 3D positions of points from 2d correspondence
-    %   Args:
-    %       P1:     projection matrix with shape 3 x 4 for image 1
-    %       pts1:   coordinates of points with shape N x 2 on image 1
-    %       P2:     projection matrix with shape 3 x 4 for image 2
-    %       pts2:   coordinates of points with shape N x 2 on image 2
-    %
-    %   Returns:
-    %       Pts3d:  coordinates of 3D points with shape N x 3
-    %
-
-    num_points = size(pts1, 1);
-    P =[];
-    A =[];
-
-    for i = 1:num_points
-        x1 = pts1(i, 1);
-        x2 = pts2(i, 1);
-        y1 = pts1(i, 2);
-        y2 = pts2(i, 2);
-        A = [
-              P1(1,:) - x1*P1(3,:);
-              P1(2,:) - y1*P1(3,:);
-              P2(1,:) - x2*P2(3,:);
-              P2(2,:) - y2*P2(3,:)
-            ];
-
-        [U, S, V] = svd(A);
-        p = V(:, end);
-        p = p ./ norm(p);
-        p = p / p(4);
-        P = [P, p];
+function pts3d = triangulate(P1, pts1, P2, pts2 )
+    N = size(pts1,1);
+    pts3d = zeros(N,3);
+    for i = 1:N
+        x1 = pts1(i,1);
+        y1 = pts1(i,2);
+        x2 = pts2(i,1);
+        y2 = pts2(i,2);
+        A = [x1 .* P1(3,:) - P1(1,:);
+             y1 .* P1(3,:) - P1(2,:);
+             x2 .* P2(3,:) - P2(1,:);
+             y2 .* P2(3,:) - P2(2,:)];
+        [~, ~, V] = svd(A);
+        v = V(:,end);
+        pts3d(i,:) = v(1:3) ./ v(4);
     end
+p1_hat = P1'*pts3d';
+p2_hat = P2'*pts3d';
+p1_hat = p1_hat(1:3,:) ./ p1_hat(4,:);
+p1_hat = p1_hat(1:2,:) ./ p1_hat(3,:);
+p1_hat = round(p1_hat);
+p2_hat = p2_hat(1:3,:) ./ p2_hat(4,:);
+p2_hat = p2_hat(1:2,:) ./ p2_hat(3,:);
+p2_hat = round(p2_hat);
 
-    P1_hat = P1 * P; % [3 * 4] * [4 * 110] = [3 * 110]
-    P1_hat = P1_hat(1:2,:) ./ P1_hat(3,:);
-    P2_hat = P2 * P; % [3 * 4] * [4 * 110] = [3* 110]
-    P2_hat = P2_hat(1:2,:) ./ P2_hat(3,:);
-    e1 = (pts1 - P1_hat').^2;
-    e1 = sqrt(sum(e1(:))) / num_points
-    e2 = (pts2 - P2_hat').^2;
-    e2 = sqrt(sum(e2(:))) / num_points
-    pts3d = P(1:3, :);
-end 
+error1 = (pts1-p1_hat').^2;
+error2 = (pts2-p2_hat').^2;
+err1 = sqrt(mean(error1(:))) / N
+err2 = sqrt(mean(error2(:))) / N
+
+end
