6767da3e5d67492e2c9fd8b2aa6e1fc216fcfca0
Ali Gholami <gholami@sfu.ca>
2020-03-03 16:11:16 -0800

    Update: Completed triangulation

--- assignment-3/matlab/testTempleCoords.m
+++ assignment-3/matlab/testTempleCoords.m
@@ -1,7 +1,36 @@
-% A test script using templeCoords.mat
-%
-% Write your code here
-%
+im1=imread('./data/im1.png');
+im2=imread('./data/im2.png');
+load('./data/someCorresp.mat')
 
-% save extrinsic parameters for dense reconstruction
-save('../data/extrinsics.mat', 'R1', 't1', 'R2', 't2');
+F = eightpoint(pts1, pts2, 640);
+load('./data/templeCoords.mat')
+pts2 = epipolarCorrespondence(im1, im2, F, pts1);
+load('./data/intrinsics.mat')
+E = essentialMatrix(F, K1, K2);
+P1 = K1 * [1 0 0 0; 0 1 0 0; 0 0 1 0];
+
+m2s = camera2(E);
+for i=1:4
+    cnt(i)=0;
+    P2 = K2 * m2s(:, :, i);
+    X(:, :, i) = triangulate(P1, pts1, P2, pts2);
+    temp = X(:, :, i);
+    cnt(i) = sum(temp(:, 3) > 0);
+end
+
+awesome_triangulation = X(:, :, find(cnt == max(cnt)));
+
+scatter3(awesome_triangulation(:, 1), awesome_triangulation(:, 2), awesome_triangulation(:, 3), 'filled')
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+
+R1 = P1(1:3, 1:3);
+t1 = P1(:, end);
+
+P2 = K2 * m2s(:, :, find(cnt==max(cnt)));
+
+R2 = P2(1:3, 1:3);
+t2 = P2(:, end);
+
+save('./data/extrinsics.mat', 'R1', 'R2', 't1', 't2')
--- assignment-3/matlab/triangulate.m
+++ assignment-3/matlab/triangulate.m
@@ -1,31 +1,32 @@
-function pts3d = triangulate(P1, pts1, P2, pts2 )
-    N = size(pts1,1);
-    pts3d = zeros(N,3);
-    for i = 1:N
-        x1 = pts1(i,1);
-        y1 = pts1(i,2);
-        x2 = pts2(i,1);
-        y2 = pts2(i,2);
-        A = [x1 .* P1(3,:) - P1(1,:);
-             y1 .* P1(3,:) - P1(2,:);
-             x2 .* P2(3,:) - P2(1,:);
-             y2 .* P2(3,:) - P2(2,:)];
-        [~, ~, V] = svd(A);
-        v = V(:,end);
-        pts3d(i,:) = v(1:3) ./ v(4);
+function pts3d = triangulate(P1, pts1, P2, pts2)
+    N = size(pts1, 1);
+    
+    pts1 = [pts1'; ones(1, N)];
+    pts2 = [pts2'; ones(1, N)];
+    P = zeros(4, N);
+    
+    for i=1:N
+        x = [0 pts1(3,i) -pts1(2,i); -pts1(3,i) 0 pts1(1,i); pts1(2,i) -pts1(1,i) 0];
+        y = [0 pts2(3,i) -pts2(2,i); -pts2(3,i) 0 pts2(1,i); pts2(2,i) -pts2(1,i) 0];
+    
+        Q = [x * P1; y * P2];
+        [U, S, V] = svd(Q);
+        z = V(:, end);
+        P(:,i) = z / z(4);
     end
-p1_hat = P1'*pts3d';
-p2_hat = P2'*pts3d';
-p1_hat = p1_hat(1:3,:) ./ p1_hat(4,:);
-p1_hat = p1_hat(1:2,:) ./ p1_hat(3,:);
-p1_hat = round(p1_hat);
-p2_hat = p2_hat(1:3,:) ./ p2_hat(4,:);
-p2_hat = p2_hat(1:2,:) ./ p2_hat(3,:);
-p2_hat = round(p2_hat);
+    
+    pts1_hat = P1 * P;
+    pts2_hat = P2 * P;
+    
+    pts3d = P(1:3,:)';
 
-error1 = (pts1-p1_hat').^2;
-error2 = (pts2-p2_hat').^2;
-err1 = sqrt(mean(error1(:))) / N
-err2 = sqrt(mean(error2(:))) / N
+    pts1 = pts1 ./ norm(pts1);
+    pts2 = pts2 ./ norm(pts2);
+    pts1_hat = pts1_hat ./ norm(pts1_hat);
+    pts2_hat = pts2_hat ./ norm(pts2_hat);
+    d1 = pts1-pts1_hat;
+    d2 = pts2-pts2_hat;
+    error1 = mean(sqrt(sum(d1.^2, 1)), 'all')
+    error2 = mean(sqrt(sum(d2.^2, 1)), 'all')
 
 end
